273429cfe4be4b0a4230c060cebf3a9b04cc2ba7
Aleksandr Dontsov <AlexDontsov2008@ya.ru>
2019-01-06 18:07:52 +0300

    Added a calculation of the factorial(6)

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -3,5 +3,6 @@
 int main(int argc, char* argv[]) {
 	print("Learn make-files");
 	std::cout << "factorial(5): " << factorial(5) << std::endl;
+	std::cout << "factorial(6): " << factorial(6) << std::endl;
 	return 0;
 }
